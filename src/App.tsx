3a08bb2b67335cd30c751dcedc574b6c1e7d210d
saidugurlu <ugurlumsaid@gmx.de>
2022-08-22 10:23:16 +0200

    frontend finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import "./App.scss";
+import { NavLink, Route, Routes, Navigate } from "react-router-dom";
+import { PageWelcome } from "./pages/PageWelcome";
+import { PageMembers } from "./pages/PageMembers";
+import { PageRegister } from "./pages/PageRegister";
+import { PageLogin } from "./pages/PageLogin";
+import { PageLogout } from "./pages/PageLogout";
+import { PageConfirmLink } from "./pages/PageConfirmLink";
+
+
 
 function App() {
   return (
-    <h1>
-      Create TypeScript React site with session-cookie
-      authentication/authorization and email registration
-    </h1>
+    <div className="App">
+      <h1>Language Tandem Group</h1>
+      <nav>
+        <NavLink to="/welcome">Welcome</NavLink>
+        <NavLink to="/members">Members</NavLink>
+        <NavLink to="/register">Register</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/logout">Logout</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/welcome" element={<PageWelcome />} />
+        <Route path="/members" element={<PageMembers />} />
+        <Route path="/register" element={<PageRegister />} />
+        <Route path="/login" element={<PageLogin />} />
+        <Route path="/logout" element={<PageLogout />} />
+        <Route path="/confirm-link" element={<PageConfirmLink />} />
+        <Route path="/" element={<Navigate to="/welcome" replace />} />
+      </Routes>
+    </div>
   );
 }
 
